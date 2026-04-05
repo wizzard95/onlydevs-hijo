86877a45984603c535398f89b954872fc6b2495e
wizzard95 <damian110995a@gmail.com>
2026-04-05 14:47:18 -0400

    config|ui: crear carpeta de componentes y carpeta de sidebar para almacenar el archivo sidebar.jsx y llamarlo desde el mainlayout

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,7 @@
+export const Sidebar = () => {
+    return (
+        <div className="h-screen bg-amber-300 text-black">
+            <span>Sidebar</span>
+        </div>
+    )
+}
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
-
+import { Sidebar } from "../components/Sidebar/Sidebar";
 
 export const MainLayout = () => {
     return(
         <div className="h-screen bg-amber-300 text-black">
-            <span>MainLayout</span>
+            <Sidebar />
             <Outlet />
         </div>
     )
