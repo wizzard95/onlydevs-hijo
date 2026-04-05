dd1c2f35ddb65dd9f689a16a92483abbfaf84667
wizzard95 <damian110995a@gmail.com>
2026-04-05 14:06:28 -0400

    config|ui(router.jsx): importar layouts dentro del archivo y hacer las respectivas llamadas configurando los enlaces a traves del enrrutamiento

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
+import { MainLayout } from "../layouts/MainLayout";
 
 export function MyRoutes () {
     return (
         <BrowserRouter>
         <Routes>
-            <Route path="/" element={<HomePage />}/>
+            <Route path="/" element={<MainLayout />}>
+                <Route index element={<HomePage />}/>
+            </Route> 
         </Routes>
         </BrowserRouter>
     )
