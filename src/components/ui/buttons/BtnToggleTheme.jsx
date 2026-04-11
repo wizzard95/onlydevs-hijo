2c2582388d52e7f8ad5eeb61a2ab3451f28d332c
wizzard95 <damian110995a@gmail.com>
2026-04-11 17:40:37 -0400

    config|ui(BtnToggleTheme.jsx): importar la funcion usethemestore para poder acceder al cambio de tema claro/oscuro

--- src/components/ui/buttons/BtnToggleTheme.jsx
+++ src/components/ui/buttons/BtnToggleTheme.jsx
@@ -1,9 +1,17 @@
+import { useThemeStore } from "../../../store/ThemeStore";
+
 export const BtnToggleTheme = () => {
+
+    const {theme, setTheme} = useThemeStore()
+
     return (
         <button className="flex items-center gap-3 p-2 rounded-lg
         hover:bg-gray-100 dark:hover:bg-primary/20 transition-all
-        justify-center cursor-pointer">
-            <span>BtnToggleTheme</span>
+        justify-center cursor-pointer"
+        onClick={setTheme}
+        >
+            <span>{theme === "light" ? "☀️" : "🌑"}</span>
+            <span>{theme === "light" ? "Claro" : "Oscuro"}</span>
         </button>
     );
 };
