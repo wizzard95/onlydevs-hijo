533646410d82dc00ec80d374379661b6328a1f19
wizzard95 <damian110995a@gmail.com>
2026-04-11 17:55:51 -0400

    config|ui(App.jsx): importarla funcion usethemestore y hacer uso de ella a traves del DOM

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { MyRoutes } from './routes/router'
+import { useThemeStore } from './store/ThemeStore'
 
 function App() {
+
+  const {theme} = useThemeStore();
+  document.documentElement.classList.toggle("dark", theme === "dark")
+
 
   const queryClient = new QueryClient()
 
