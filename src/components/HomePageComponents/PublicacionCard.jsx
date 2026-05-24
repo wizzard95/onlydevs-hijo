103fda7b2673748bdf98508e6254ea5b72b69645
wizzard95 <damian110995a@gmail.com>
2026-05-24 12:27:22 -0400

    feat|ui: agregar archivo prettierrc para formatear la posicion(estructura del codigo)| crear la card donde iran las publicaciones y estilizarla

---
+++ src/components/HomePageComponents/PublicacionCard.jsx
@@ -0,0 +1,28 @@
+import { Icon } from '@iconify/react'
+
+export const PublicacionCard = () => {
+    return (
+        <div className="border-b border-gray-500/50 p-4">
+            <div className="flex justify-between">
+                <div className="flex items-center gap-3">
+                    <img
+                        src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/f5/f8/67/photo1jpg.jpg?w=2000&h=-1&s=1"
+                        className="w-12 h-12 rounded-full object-cover"
+                    />
+                    <span className="font-bold">nombre de usuario</span>
+                </div>
+                <div className="flex items-center gap-2">
+                    <span className="text-gray-500 text-sm whitespace-nowrap">
+                        hace 8h
+                    </span>
+                    <button>
+                        <Icon
+                            icon="mdi:dots-horizontal"
+                            className="text-gray-500"
+                        />
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,27 @@
-import { Icon } from "@iconify/react"
-import { HeaderSticky } from "../components/HomePageComponents/HeaderSticky"
-import { InputPublicar } from "../components/HomePageComponents/InputPublicar"
+import { Icon } from "@iconify/react";
+import { HeaderSticky } from "../components/HomePageComponents/HeaderSticky";
+import { InputPublicar } from "../components/HomePageComponents/InputPublicar";
+import { PublicacionCard } from "../components/HomePageComponents/PublicacionCard";
 
 export const HomePage = () => {
-    return (
-        <main className="flex min-h-screen bg-white dark:bg-bg-dark
-        max-w-[1200] mx-auto">
-            
-            <section className="flex flex-col w-full h-screen">
-             
-                <article className="flex flex-col h-screen overflow-hidden border
-                border-gray-200 border-t-0 border-b-0 dark:border-gray-600">
-                    <HeaderSticky />
-                    <div className="overflow-y-auto">
-                    <InputPublicar />
-                    </div>
-                    
-                        Principal
-                </article>
-                <article>Sidebar derecho</article>
-            </section>
-
-        </main>
-    )
-}
+  return (
+    <main
+      className="flex min-h-screen bg-white dark:bg-bg-dark
+        max-w-[1200] mx-auto"
+    >
+      <section className="flex flex-col w-full h-screen">
+        <article
+          className="flex flex-col h-screen overflow-hidden border
+                border-gray-200 border-t-0 border-b-0 dark:border-gray-600"
+        >
+          <HeaderSticky />
+          <div className="overflow-y-auto">
+            <InputPublicar />
+            <PublicacionCard />
+          </div>
+        </article>
+        <article>Sidebar derecho</article>
+      </section>
+    </main>
+  );
+};
