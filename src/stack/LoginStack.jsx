b8a18df5ac881fc744171870ffebb0a3e302bd1e
wizzard95 <damian110995a@gmail.com>
2026-05-27 00:08:46 -0400

    feat|ui: crear ../LoginStack para manejar el crear usuario y el inicio de sesion

---
+++ src/stack/LoginStack.jsx
@@ -0,0 +1,23 @@
+import { useMutation } from '@tanstack/react-query'
+import { useAuthStore } from '../store/AuthStore'
+import { toast } from 'react-hot-toast'
+
+export const useCrearUsuarioYSesionMutate = () => {
+    const { crearUserYLogin, credenciales } = useAuthStore()
+
+    return useMutation({
+        mutationKey: ['iniciar con email tester'],
+        mutationFn: async () => {
+            await crearUserYLogin({
+                email: credenciales.email,
+                password: credenciales.password,
+            })
+        },
+        onError: (error) => {
+            toast.error(`Error: ${error.message}`)
+        },
+        onSuccess: () => {
+            toast.success('ok, todo salio bien')
+        },
+    })
+}
