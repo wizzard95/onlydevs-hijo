be1323220833f92d47141870b68274b1f0a8b6fd
wizzard95 <damian110995a@gmail.com>
2026-05-28 14:14:51 -0400

    feat|ui: agregar evento showPassword al input de la contraseña para ver y ocultarla

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -57,7 +57,7 @@ export const LoginPage = () => {
                         </div>
                         <div className="relative mb-4">
                             <input
-                                type="password"
+                                type={showPassword ? 'text' : 'password'}
                                 placeholder="Contraseña"
                                 className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:outline-none
                                 focus:ring-2 focus:ring-[#1d93ba]"
--- src/stack/LoginStack.jsx
+++ src/stack/LoginStack.jsx
@@ -1,6 +1,6 @@
 import { useMutation } from '@tanstack/react-query'
 import { useAuthStore } from '../store/AuthStore'
-import { toast } from 'react-hot-toast'
+import { toast } from 'sonner'
 
 export const useCrearUsuarioYSesionMutate = () => {
     const { crearUserYLogin, credenciales } = useAuthStore()
