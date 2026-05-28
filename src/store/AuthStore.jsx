6d7018480aa8d06a0cdb568d36b2cfbc6bac049d
wizzard95 <damian110995a@gmail.com>
2026-05-28 19:33:15 -0400

    ui|Auth: crear funcion para mantener la session de usuario sin la necesidad de tener que estar iniciando sesion a cada rato

--- src/store/AuthStore.jsx
+++ src/store/AuthStore.jsx
@@ -13,3 +13,25 @@ export const useAuthStore = create((set) => ({
         return data.user
     },
 }))
+
+export const useSubcription = create((set) => {
+    // *iniciar el estado
+    const store = {
+        user: null,
+        setUser: (user) => set({ user }),
+    }
+    //* Listener que se ejecuta una vez cuando se importa el store
+    supabase.auth.getSession().then(({ data: { session } }) => {
+        if (session?.user) {
+            set({ user: sessionStorage.user })
+        }
+    })
+    supabase.auth.onAuthStateChange((_event, session) => {
+        if (session?.user) {
+            set({ user: session.user })
+        } else {
+            set({ user: null })
+        }
+    })
+    return store
+})
